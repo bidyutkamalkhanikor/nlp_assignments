5e54efe98ad4a69db32405aa5bf71e02f57173f0
Bidyut Kamal Khanikor <bidyutkamalkhanikor1@gmail.com>
2019-02-27 23:13:54 +0530

    my first assignment

---
+++ assignment1.py
@@ -0,0 +1,16 @@
+"""
+Created on Wed Feb 27 22:24:01 2019
+
+@author: bidyut
+"""
+
+import nltk
+from nltk.corpus import brown
+
+cfd = nltk.ConditionalFreqDist(
+    (genre, word)
+    for genre in brown.categories()
+    for word in brown.words(categories=genre))
+genres = ['news', 'religion', 'hobbies', 'science_fiction', 'romance', 'humor']
+modals = ['can', 'could', 'may', 'might', 'must', 'will']
+cfd.tabulate(conditions=genres, samples=modals)
